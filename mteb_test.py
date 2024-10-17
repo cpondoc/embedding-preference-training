3980d1ab3eb8df6af352a0d0b40f619b8fa1f391
Chris Pondoc <chris.pondoc@comcast.net>
2024-10-16 23:17:34 -0700

    Try extracting the custom model

--- mteb_test.py
+++ mteb_test.py
@@ -1,28 +1,54 @@
+"""
+Code to benchmark some models on retrieval tasks.
+"""
 from mteb import MTEB
 import mteb
 from sentence_transformers import SentenceTransformer
+from models.fineweb_noclass import *
 import torch
 
+# For safe measure
 torch.cuda.empty_cache()
-
-
-# Define model names
 MODEL_NAMES = ["HuggingFaceFW/ablation-model-fineweb-edu", "Snowflake/snowflake-arctic-embed-m", "HuggingFaceFW/fineweb-edu-classifier"]
-MODEL_NAMES = [MODEL_NAMES[1]]
 TASKS = ["AlphaNLI"]
 
-# Iterate through models, load them in using SentenceTransformer, add padding
-for model_name in MODEL_NAMES:
-    model = SentenceTransformer(model_name)
-    if model_name == "HuggingFaceFW/fineweb-edu-classifier":
-        model.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    else:
-        model.tokenizer.pad_token = model.tokenizer.eos_token
+def run_baseline_models():
+    """
+    Run some of the already accessible models on Hugging Face.
+    """
+    # Initialize a SentenceTransformer for each model
+    for model_name in MODEL_NAMES:
+        model = SentenceTransformer(model_name)
+        
+        # Add a padding token if we need it
+        if model_name == "HuggingFaceFW/fineweb-edu-classifier":
+            model.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        else:
+            model.tokenizer.pad_token = model.tokenizer.eos_token
 
-    # Define tasks
+        # Define tasks, run eval
+        tasks = mteb.get_tasks(tasks=TASKS, languages=["eng"])
+        evaluation = MTEB(tasks=tasks)
+        evaluation.run(
+            model,
+            eval_splits=["test"],
+            save_predictions=True,
+            output_folder="results",
+            encode_kwargs={
+                'batch_size': 4,
+            }
+        )
+
+def run_custom_model():
+    """
+    Try to run on classifier without the classification head, so just the embedding model.
+    """
+    # Define custom model
+    custom_model = SnowflakeModelWithoutClassifier()
+    model = SentenceTransformer(modules=[custom_model])
+    
+    # Define tasks, run evaluation
     tasks = mteb.get_tasks(tasks=TASKS, languages=["eng"])
-
-    # Run evaluation of embedding model on retrieval tasks
     evaluation = MTEB(tasks=tasks)
     evaluation.run(
         model,
@@ -30,6 +56,8 @@ for model_name in MODEL_NAMES:
         save_predictions=True,
         output_folder="results",
         encode_kwargs={
-            'batch_size': 8,
+            'batch_size': 4,
         }
     )
+    
+run_custom_model()
