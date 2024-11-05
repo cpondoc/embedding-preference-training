bddd93b7092863cf4a24541117ff18a068d4b572
Chris Pondoc <chris.pondoc@comcast.net>
2024-11-04 22:53:45 -0800

    Finish loading in all of the data

--- wiki_trafila_test.py
+++ wiki_trafila_test.py
@@ -1,6 +1,7 @@
 # import the necessary functions
 import requests
-from trafilatura import fetch_url, extract
+from trafilatura import fetch_url, extract, html2txt
+from tqdm import tqdm
 from urllib.parse import unquote
 
 
@@ -53,24 +54,63 @@ def get_good_articles():
     """
     Get all of the good Wikipedia articles.
     """
-    urls, titles = [], set()
+    urls, titles = [], []
     with open("data/wiki/good-articles.txt", "r", encoding="utf-8") as file:
         for line in file:
             url = line.split(" ")[0]
             page_url = f"https://en.wikipedia.org{url}"
             urls.append(page_url)
-            titles.add(get_wikipedia_title_from_url(url))
+            titles.append(get_wikipedia_title_from_url(url))
 
     return urls, titles
 
+def get_bad_articles(good_titles):
+    """
+    Get random Wikipedia articles.
+    """
+    urls, titles = [], set()
+    while len(titles) < len(good_titles):
+        new_rando, new_url = get_random_wikipedia_page()
+        if new_rando not in good_titles:
+            titles.add(new_rando)
+            urls.append(new_url)
+            
+            with open("data/bad-wiki.txt", "a") as file:
+                file.write(f"{new_url} - {new_rando}\n")
+        print(len(good_titles) - len(titles))
+    return urls, titles
+
+def read_bad_articles():
+    """
+    Read from file.
+    """
+    urls, titles = [], []
+    with open("data/bad-wiki.txt", "r", encoding="utf-8") as file:
+        for line in file:
+            url = line.split(" ")[0]
+            urls.append(url)
+            titles.append(get_wikipedia_title_from_url(url))
+
+    return urls, titles
+    
+def save_to_file(content, article_type, title):
+    """
+    Save content to a text file.
+    """
+    with open("data/wiki-vs/" + article_type + "/" + title + ".txt" , "w", encoding="utf-8") as file:
+        if content:
+            file.write(content)
 
 if __name__ == "__main__":
-    urls, titles = get_good_articles()
-    bad_urls, bad_titles = [], set()
-    while len(bad_titles) < len(titles):
-        new_rando, new_url = get_random_wikipedia_page()
-        if new_rando not in titles:
-            bad_titles.add(new_rando)
-            bad_urls.append(new_url)
-        print(new_url)
-    print(bad_titles)
+    good_urls, good_titles = get_good_articles()
+    bad_urls, bad_titles = read_bad_articles()
+    for i in tqdm(range(len(good_urls))):
+        url, title = good_urls[i], good_titles[i]
+        content = use_trafilatura(url)
+        save_to_file(content, "good", title)
+    
+    for i in tqdm(range(len(bad_urls))):
+        url, title = bad_urls[i], bad_titles[i]
+        content = use_trafilatura(url)
+        save_to_file(content, "bad", title)
+    # bad_urls, bad_titles = get_bad_articles(good_titles)
