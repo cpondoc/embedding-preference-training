b6b31df94b0f582e2e0dea8e02b35ee82dbf6eb6
Chris Pondoc <chris.pondoc@comcast.net>
2024-11-20 00:11:26 -0800

    Make it more adjustable

--- code/eval/rerank.py
+++ code/eval/rerank.py
@@ -7,23 +7,30 @@ import mteb
 from sentence_transformers import SentenceTransformer
 
 # Define reranker, set of base models that we can change
-BASE_MODELS = ["Snowflake/snowflake-arctic-embed-m"]
+BASE_MODEL = "Snowflake/snowflake-arctic-embed-m"
+QUALITY_MODELS = {
+    "HuggingFaceTB/fineweb-edu-classifier": "fineweb-edu",
+    "/home/cpondoc/research/embedding-preference-training/scratch/sample-run/final": "gb_wiki",
+}
 
 # Also define the set of tasks
 TASKS = ["ArguAna", "FiQA2018", "QuoraRetrieval"]
 tasks = mteb.get_tasks(tasks=TASKS, languages=["eng"])
 
 # Iterate through each model, set up the initial encoder
-for base_model in BASE_MODELS:
-    dual_encoder = SentenceTransformer(base_model)
-    eval_splits = ["test"]
+for quality_p in [0.95, 0.9, 0.85, 0.8]:
+    for key, value in QUALITY_MODELS.items():
+        dual_encoder = SentenceTransformer(BASE_MODEL)
+        eval_splits = ["test"]
 
-    # Run eval by first doing the results, then reranking
-    for task in TASKS:
-        evaluation = MTEB(tasks=[task])
-        evaluation.run(
-            dual_encoder,
-            eval_splits=eval_splits,
-            save_predictions=True,
-            output_folder="results/gb-wiki-scores/" + base_model,
-        )
+        # Run eval by first doing the results, then reranking
+        for task in TASKS:
+            evaluation = MTEB(tasks=[task])
+            evaluation.run(
+                dual_encoder,
+                eval_splits=eval_splits,
+                save_predictions=True,
+                output_folder="results/" + value + "/" + str(quality_p),
+                quality_p = quality_p,
+                quality_classifier = key,
+            )
