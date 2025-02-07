0244137d330a3813ad42f6fa0d26608abb9674fe
Chris Pondoc <chris.pondoc@comcast.net>
2025-02-06 20:50:19 -0800

    minor

--- code/dataset/load_hf.py
+++ code/dataset/load_hf.py
@@ -4,5 +4,6 @@ Saving example for future use.
 
 from datasets import load_dataset
 
-dataset = load_dataset("cpondoc/noisy-cc", use_auth_token=True)
+dataset = load_dataset("cpondoc/noisy-cc", data_files={"train": "data/*.csv"}, ignore_verifications=True, keep_in_memory=True)
 train_data = dataset["train"]
+print(train_data[:20]) # Get first 20 examples
